21812846e7a0357639afdc274fd4ac0fe023e813
randomStudentES <jlirascand1@educacion.navarra.es>
2025-04-14 21:55:35 +0200

    Triste pero cierto :(

--- src/main/java/es/masanz/ut7/pokemonfx/model/type/Fuego.java
+++ src/main/java/es/masanz/ut7/pokemonfx/model/type/Fuego.java
@@ -2,6 +2,6 @@ package es.masanz.ut7.pokemonfx.model.type;
 
 public interface Fuego {
     default void quemar(){
-        System.out.println("Has quemado al pokemon enemigo");
+        System.out.println("Has quemado al pokemon enemigo"); // admito que no sabia que habia que hacer en interfaces así que mire que hizo fran
     }
 }
